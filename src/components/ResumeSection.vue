3705e075aa024892ce28fe0788e9f2bde65967c8
ThuthuzaPoswayo <thuthuzaposwayo@gmail.com>
2024-07-04 13:19:29 +0200

    Testimonials

--- src/components/ResumeSection.vue
+++ src/components/ResumeSection.vue
@@ -1,68 +1,74 @@
 <template>
   <div class="container">
-   <div class="row">
-    <div id="resume">
-<h1 class="display-1">Resume</h1>
+    <div class="row">
+      <div id="resume">
+        <h1 class="display-1">Resume</h1>
 
-  <!--Education Section-->
-<section>
-  <h3 class="display-2">Education</h3>
-  <div class ="row gap-4" v-if="education?.length">
-   
-<CardComp v-for="item in education"   :key="item.id">
-  <template>
-  <img src="" alt="academy-img"/>
+        <!--Education Section-->
+        <div>
+          <h3 class="display-2">Education</h3>
+          <div class="row gap-4" v-if="education">
+            <CardComp v-for="(educ, id) in education" :key="id">
+              <template #cardHeader>
+                <h3 class="heading">{{ edueduccation.institution }}</h3>
+                <!-- <img src="" alt="academy-img"/> -->
+              </template></CardComp
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
-<template>
-  <p class="heading">{{item.institution}}</p>
-  <p>{{item.qualification}}</p>
-  <p>{{item.year}}</p>
+
+<template #cardBody>
+  <p>{{ educ.qualification }}</p>
+  <p>{{ educ.year }}</p>
 </template>
 </CardComp>
 </div>
 <Spinner  v-else />
-</section>
+</div>
 
 <!-- Skills -->
 
-<section>
+<div>
   <h3 class="display-2">Skills</h3>
   <div class ="row gap-4" v-if="skills?.length">
    
 <CardComp v-for="item in skills"   :key="item.id">
   <template>
-  <img src="" alt="academy-img"/>
+  <img src="" alt="academy-img" />
 </template>
 <template>
-  <p class="heading">{{item.skillName1}}</p>
-  <p>{{item.skillName2}}</p>
-  <p>{{item.skillName}}</p>
+  <p class="heading">{{ item.skillName1 }}</p>
+  <p>{{ item.skillName2 }}</p>
+  <p>{{ item.skillName }}</p>
 </template>
 </CardComp>
 </div>
 <Spinner  v-else />
-</section>
+</div>
 
 
 <!-- Experience -->
-<section>
+<div>
   <h3 class="display-2">Experience</h3>
   <div class ="row gap-4" v-if="experience?.length">
    
 <CardComp v-for="item in experience"   :key="item.id">
   <template>
-  <img src="" alt="academy-img"/>
+  <img src="" alt="academy-img" />
 </template>
 <template>
-  <p class="heading">{{item.jobTitle}}</p>
-  <p>{{item.year}}</p>
-  <p>{{item.description}}</p>
+  <p class="heading">{{ item.jobTitle }}</p>
+  <p>{{ item.year }}</p>
+  <p>{{ item.description }}</p>
 </template>
 </CardComp>
 </div>
 <Spinner  v-else />
-</section>
-
+</div>
 
 
 
@@ -81,23 +87,20 @@
 
 <script setup>
 import Spinner from "./Spinner.vue";
-import { computed, onMounted} from "vue";
+import { computed, onMounted } from "vue";
 import { useStore } from "vuex";
 
 const store = useStore();
-const education = computed(()  => store.state.education);
-const skills = computed (()    =>  store.state.skills);
-const experience = computed(()   =>  store.state.experience)
+const education = computed(() => store.state.education);
+const skills = computed(() => store.state.skills);
+const experience = computed(() => store.state.experience);
 
-onMounted(()  => {
-    store.dispatch("fetcheducation")
-    store.dispatch("fetchskills")
-    store.dispatch("fetchexperience")
-})
-
-
+onMounted(() => {
+  store.dispatch("fetchEducation");
+  store.dispatch("fetchSkills");
+  store.dispatch("fetchexperience");
+});
 </script>
 
 <style>
-
 </style>
